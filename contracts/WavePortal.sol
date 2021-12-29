e3a296f213232ead552eeece6c5c795798bc4e31
Muskulee <muskulee@gmail.com>
2021-12-29 14:18:46 +0100

    Add ability to wave, and store waver's address

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,40 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+
+    uint256 totalWaves;
+
+    string[] totalWavers;
+
     constructor(){
         console.log("Hello World, I am excited about building and implementing my SmartContract");
     }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved", msg.sender);
+
+    }
+
+    function getTotalWaves() public view returns(uint256){
+        console.log("We have %d Total Waves", totalWaves);
+        return totalWaves;
+    }
+
+
+    function getTotalWavers() public view returns(string[] memory){
+
+        return totalWavers;
+    }
+
+    function storeNewWaver(string calldata _waver) public {
+        totalWavers.push(_waver);
+        console.log("%s Saved", _waver);
+    }
+
+
+   
+
+
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,54 @@
 const main = async () => {
+
+
+    
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+     waveCount = await waveContract.getTotalWaves();
+
+     let waveStorage;
+
+        waveStorage = await waveContract.getTotalWavers()
+
+
+     let waveTxn = await waveContract.wave();
+
+        await waveTxn.wait();
+
+
+     let storeNew = await  waveContract.storeNewWaver(owner.address)
+
+        await storeNew.wait();
+
+
+
+     waveCount = await waveContract.getTotalWaves();
+
+     waveStorage = await waveContract.getTotalWavers()
+
+     waveTxn = await waveContract.connect(randomPerson).wave();
+        await waveTxn.wait();
+
+    storeNew = await waveContract.connect(randomPerson).storeNewWaver(randomPerson.address);
+        await storeNew.wait();
+
+     
+     
+
+     waveCount = await waveContract.getTotalWaves();
+     waveStorage = await waveContract.getTotalWavers();
+
+     console.log(`waveStorage`, waveStorage)
+     console.log(`randomPerson`, randomPerson.address)
+
+
+
 }
 
 
