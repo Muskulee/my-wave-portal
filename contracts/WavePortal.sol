f55454929672fa84a8f7fa44956c0e137d8230ea
Muskulee <muskulee@gmail.com>
2022-01-24 10:52:46 +0100

    Lesson 4: Give each user a tokne(0.001ether)

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,40 +5,70 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
-
     uint256 totalWaves;
 
-    string[] totalWavers;
+    //event are intermediates for logging transactions easily
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
-    constructor(){
-        console.log("Hello World, I am excited about building and implementing my SmartContract");
+    //a struct is a customisable datatype
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+    // string[] totalWavers;
+
+    //declare wave array for storing waves and its message
+
+    Wave[] waves;
+
+    constructor() payable {
+        console.log(
+            "Hello World, I am excited about building and implementing my SmartContract"
+        );
+        console.log(
+            "Now Edited and it is in its final phase"
+        );
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved", msg.sender);
+        console.log("%s has waved w/ message", msg.sender, _message);
+
+    waves.push(Wave(msg.sender, _message, block.timestamp));
+
+    emit NewWave(msg.sender, block.timestamp, _message);
+
+
+//section 4 lesson for gifting users ethers
+
+    uint256 prizeAmount = 0.0001 ether;
+    require(prizeAmount <= address(this).balance, 
+    "Trying to withdraw more money than the contract has.");
+    
+    (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+    require(success, "Failed to withdraw from contact");
+
 
     }
 
-    function getTotalWaves() public view returns(uint256){
+    //add a new function to get all Waves
+
+    function getAllWaves() public view returns (Wave[] memory){
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
         console.log("We have %d Total Waves", totalWaves);
         return totalWaves;
     }
 
+    // function getTotalWavers() public view returns (string[] memory) {
+    //     return totalWavers;
+    // }
 
-    function getTotalWavers() public view returns(string[] memory){
-
-        return totalWavers;
-    }
-
-    function storeNewWaver(string calldata _waver) public {
-        totalWavers.push(_waver);
-        console.log("%s Saved", _waver);
-    }
-
-
-   
-
-
-
+    // function storeNewWaver(string calldata _waver) public {
+    //     totalWavers.push(_waver);
+    //     console.log("%s Saved", _waver);
+    // }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,70 +1,160 @@
-const main = async () => {
+// const main = async () => {
 
 
     
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
+//     const [owner, randomPerson] = await hre.ethers.getSigners();
+//     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+//     const waveContract = await waveContractFactory.deploy();
+//     await waveContract.deployed();
+//     console.log("Contract deployed to:", waveContract.address);
+//     console.log("Contract deployed by:", owner.address);
 
-    let waveCount;
-     waveCount = await waveContract.getTotalWaves();
+//     let waveCount;
+//      waveCount = await waveContract.getTotalWaves();
 
-     let waveStorage;
+//      let waveStorage;
 
-        waveStorage = await waveContract.getTotalWavers()
-
-
-     let waveTxn = await waveContract.wave();
-
-        await waveTxn.wait();
+//         waveStorage = await waveContract.getTotalWavers()
 
 
-     let storeNew = await  waveContract.storeNewWaver(owner.address)
+//      let waveTxn = await waveContract.wave();
 
-        await storeNew.wait();
+//         await waveTxn.wait();
+
+
+//      let storeNew = await  waveContract.storeNewWaver(owner.address)
+
+//         await storeNew.wait();
 
 
 
-     waveCount = await waveContract.getTotalWaves();
+//      waveCount = await waveContract.getTotalWaves();
 
-     waveStorage = await waveContract.getTotalWavers()
+//      waveStorage = await waveContract.getTotalWavers()
 
-     waveTxn = await waveContract.connect(randomPerson).wave();
-        await waveTxn.wait();
+//      waveTxn = await waveContract.connect(randomPerson).wave();
+//         await waveTxn.wait();
 
-    storeNew = await waveContract.connect(randomPerson).storeNewWaver(randomPerson.address);
-        await storeNew.wait();
+//     storeNew = await waveContract.connect(randomPerson).storeNewWaver(randomPerson.address);
+//         await storeNew.wait();
 
      
      
 
-     waveCount = await waveContract.getTotalWaves();
-     waveStorage = await waveContract.getTotalWavers();
+//      waveCount = await waveContract.getTotalWaves();
+//      waveStorage = await waveContract.getTotalWavers();
 
-     console.log(`waveStorage`, waveStorage)
-     console.log(`randomPerson`, randomPerson.address)
+//      console.log(`waveStorage`, waveStorage)
+//      console.log(`randomPerson`, randomPerson.address)
 
 
 
-}
+// }
 
+
+// const runMain = async () => {
+//     try{
+//         await main();
+//         process.exit(0);
+
+//     }
+
+//     catch (error) {
+//         console.log(error.message);
+//         process.exit(1);
+//     }
+// }
+
+
+
+// runMain();
+
+
+
+
+// const main = async () => {
+//     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+//     const waveContract = await waveContractFactory.deploy();
+//     await waveContract.deployed();
+//     console.log("Contract addy:", waveContract.address);
+  
+//     let waveCount;
+//     waveCount = await waveContract.getTotalWaves();
+//     console.log(waveCount.toNumber());
+  
+//     /**
+//      * Let's send a few waves!
+//      */
+//     let waveTxn = await waveContract.wave("A message!");
+//     await waveTxn.wait(); // Wait for the transaction to be mined
+  
+//     const [_, randomPerson] = await hre.ethers.getSigners();
+//     waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+//     await waveTxn.wait(); // Wait for the transaction to be mined
+  
+//     let allWaves = await waveContract.getAllWaves();
+//     console.log(allWaves);
+//   };
+  
+//   const runMain = async () => {
+//     try {
+//       await main();
+//       process.exit(0);
+//     } catch (error) {
+//       console.log(error);
+//       process.exit(1);
+//     }
+//   };
+  
+//   runMain();
+
+
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+  console.log("Contract addy:", waveContract.address);
+
+  /*
+   * Get Contract balance
+   */
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  /*
+   * Send Wave
+   */
+  let waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait();
+
+  /*
+   * Get Contract balance to see what happened!
+   */
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
 
 const runMain = async () => {
-    try{
-        await main();
-        process.exit(0);
-
-    }
-
-    catch (error) {
-        console.log(error.message);
-        process.exit(1);
-    }
-}
-
-
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 runMain();
